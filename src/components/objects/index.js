3840342bde416c9b9edbe7168cf6ed58ce347ad6
Rik Lamers <info@riklamers.com>
2018-10-14 15:04:20 +0200

    Added checkQr code

--- src/components/objects/index.js
+++ src/components/objects/index.js
@@ -1,1 +1,2 @@
-export { default as Dropdown } from './o-dropdown/js/o-dropdown';
+// export { default as Dropdown } from './o-dropdown/js/o-dropdown';
+export { default as CheckQR } from './o-check-qr/js/o-check-qr';
