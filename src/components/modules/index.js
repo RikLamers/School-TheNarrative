19ffee9a037b3e5908637aa654c6d3d911b23147
Rimarve <rrwvanemmerik@gmail.com>
2018-10-25 15:33:10 +0200

    whatsapp messages and layout

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -3,3 +3,4 @@
 export { default as Navigation } from './m-navigation/js/m-navigation';
 export { default as MobileInterface } from './m-mobile-interface/js/m-mobile-interface';
 export { default as UserProfile } from './m-user-profile/js/m-user-profile';
+export { default as WhatsApp } from './m-whatsapp/js/m-whatsapp';
--- src/components/modules/m-whatsapp/js/m-whatsapp.js
+++ src/components/modules/m-whatsapp/js/m-whatsapp.js
@@ -12,9 +12,22 @@ class WhatsApp {
 
     eventListeners() { }
 
+    displayText() {
+        var recieved = document.getElementById("recieved_txt");
+        var introText = "Koning Clovis heeft mij gevraagd het Christendom in Maastricht te verspreiden. Daarom heb ik een afspraak gemaakt met de bisschop van Tongeren waardoor we nu zijn goede naam mogen gebruiken. We maken van hem een heilige; ‘sint Servaas’! Nu moeten we alleen nog wonderen creëren zodat de mensen ons geloven. Wil je me hierbij helpen?";
+
+        recieved.innerHTML = '<p class=\"m-whatsapp__message\">' + introText + '</p><span class=\"m-whatsapp__timestamp\">' + '</span>';
+
+
+        setTimeout(function(){
+
+            }, 300);
+    }
+
     initialize() {
         this.setup();
         this.eventListeners();
+        this.displayText();
     }
 }
 
