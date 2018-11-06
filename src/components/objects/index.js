f93d638c9d0731fe2c77d731d91dc91eed32284d
Rik Lamers <info@riklamers.com>
2018-11-06 16:56:31 +0100

    Export progression object

--- src/components/objects/index.js
+++ src/components/objects/index.js
@@ -1,2 +1,3 @@
 // export { default as Dropdown } from './o-dropdown/js/o-dropdown';
 export { default as CheckQR } from './o-check-qr/js/o-check-qr';
+export { default as Progression } from './o-progression/js/o-progression';
