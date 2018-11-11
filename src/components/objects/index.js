dabd26c331c6d19b078794f353ff12f4f840b078
Rik Lamers <info@riklamers.com>
2018-11-11 14:46:36 +0100

    Imported and exported notification

--- src/components/objects/index.js
+++ src/components/objects/index.js
@@ -1,3 +1,4 @@
 // export { default as Dropdown } from './o-dropdown/js/o-dropdown';
 export { default as CheckQR } from './o-check-qr/js/o-check-qr';
+export { default as Notification } from './o-notification/js/o-notification';
 export { default as Progression } from './o-progression/js/o-progression';
