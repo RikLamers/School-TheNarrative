21326e348f09a6ac01bd26454c4ab0013a0b20a5
Rik Lamers <info@riklamers.com>
2018-11-12 19:32:43 +0100

    Export intro module

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -1,6 +1,7 @@
 export { default as DragDrop } from './m-drag-drop/js/m-drag-drop';
 export { default as Facebook } from './m-facebook/js/m-facebook';
 export { default as Instagram } from './m-instagram/js/m-instagram';
+export { default as Intro } from './m-intro/js/m-intro';
 export { default as Navigation } from './m-navigation/js/m-navigation';
 export { default as Rebus } from './m-rebus/js/m-rebus';
 export { default as MobileInterface } from './m-mobile-interface/js/m-mobile-interface';
