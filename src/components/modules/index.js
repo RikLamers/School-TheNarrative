983dcda59a73bb2d29935dade1257cace2e6da25
Rik Lamers <info@riklamers.com>
2018-11-14 10:30:31 +0100

    Imported merovinger

--- src/components/modules/index.js
+++ src/components/modules/index.js
@@ -4,6 +4,7 @@ export { default as Facebook } from './m-facebook/js/m-facebook';
 export { default as Instagram } from './m-instagram/js/m-instagram';
 export { default as Intro } from './m-intro/js/m-intro';
 export { default as Merofy } from './m-merofy/js/m-merofy';
+export { default as Merovinger } from './m-merovinger/js/m-merovinger';
 export { default as Navigation } from './m-navigation/js/m-navigation';
 export { default as Rebus } from './m-rebus/js/m-rebus';
 export { default as MobileInterface } from './m-mobile-interface/js/m-mobile-interface';
